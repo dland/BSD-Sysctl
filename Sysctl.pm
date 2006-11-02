1d9695d318f10c69e7f15e0773f61d5b2f543f98
David Landgren <david@landgren.net>
2006-11-02 17:01:49 +0000

    The final changes for 0.07 (lost between svn and svk on slothrop)

--- Sysctl.pm
+++ Sysctl.pm
@@ -12,7 +12,7 @@ use XSLoader;
 
 use vars qw($VERSION @ISA %MIB_CACHE %MIB_SKIP @EXPORT_OK);
 
-$VERSION = '0.06';
+$VERSION = '0.07';
 @ISA     = qw(Exporter);
 
 use constant FMT_A           =>  1;
@@ -90,8 +90,8 @@ BSD::Sysctl - Manipulate kernel sysctl variables on BSD-like systems
 
 =head1 VERSION
 
-This document describes version 0.06 of BSD::Sysctl, released
-2006-10-10.
+This document describes version 0.07 of BSD::Sysctl, released
+2006-10-22.
 
 =head1 SYNOPSIS
 
